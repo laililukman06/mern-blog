3205d4f66f515b3fee9ef25af85aace4d5f03249
Laili Lukman Kerja <laililukman.kerja@gmail.com>
2024-10-09 16:27:19 +0700

    create sign in api route

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,9 +1,9 @@
 import User from "../models/user.model.js";
 import bcryptjs from "bcryptjs";
 import { errorHandler } from "../utils/error.js";
+import jwt from "jsonwebtoken";
 
 const signup = async (req, res, next) => {
-  // console.log(req.body);
   const { username, email, password } = req.body;
 
   if (
@@ -33,4 +33,35 @@ const signup = async (req, res, next) => {
   }
 };
 
-export { signup };
+const signin = async (req, res, next) => {
+  const { email, password } = req.body;
+
+  if (!email || !password || email === "" || password === "") {
+    next(errorHandler(400, "All fields are required"));
+  }
+
+  try {
+    const validUser = await User.findOne({ email });
+    if (!validUser) {
+      returnnext(errorHandler(404, "User not found"));
+    }
+    const validPassword = bcryptjs.compareSync(password, validUser.password);
+    if (!validPassword) {
+      return next(errorHandler(401, "Invalid Password"));
+    }
+    
+    const token = jwt.sign(
+      {id: validUser._id}, process.env.JWT_SECRET
+    );
+
+    const{password: pass, ...rest} = validUser._doc;
+
+    res.status(200).cookie("access_token", token, {
+      httpOnly: true,
+    }).json(rest);
+  } catch (error) {
+    next(error);
+  }
+};
+
+export { signup, signin };
